8289bb43dff8b4bebefd57883d9a6257fdaa373a
Thomas Ryan <thomasryan2118@gmail.com>
2022-09-17 23:14:12 -0600

    first doughnut work

---
+++ Pages/Doughnut.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesDoughnuts.Pages
+{
+    public class DoughnutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
