d3e64a9d1e47c4b37935069bd7c8ab9e657aa10b
Thomas Ryan <thomasryan2118@gmail.com>
2022-09-28 20:32:13 -0600

    added page to add doughnut

---
+++ Pages/doughnuts/create.cshtml.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesDoughnuts.data;
+using RazorPagesDoughnuts.Models;
+
+namespace RazorPagesDoughnuts.Pages.doughnuts;
+
+public class CreateModel : PageModel{
+
+    public void OnGet(){
+        
+    }
+}
